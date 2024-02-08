22bc44c7c090a93913a0467c33a661d0f6a354b9
margay <morgann.eriksson@proofofconcept.space>
2024-02-08 21:27:50 +0100

    Add declaration for v_SetLogLevel

--- vinland/include/vinland/log.h
+++ vinland/include/vinland/log.h
@@ -3,12 +3,11 @@
 
 enum LogLevel { DEBUG = 0, INFO = 1, WARNING = 2, ERROR = 3, FATAL = 4 };
 
-
+void v_SetLogLevel(enum LogLevel value);
 /* TODO
  * Create macro for this
  */
-void v_Log(const char *f, const int l, const char *lvl, const char *t,
-           const char *msg);
+void v_Log(const char *f, const int lvl, const int l, const char *msg);
 
 /* TODO
  * Move to internal header
