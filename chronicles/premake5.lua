5f40889fc6bee8bb8e4d57db87f7809037d407f2
margay <morgann.eriksson@proofofconcept.space>
2024-02-11 21:49:15 +0100

    Integrate raylib to other projects

--- chronicles/premake5.lua
+++ chronicles/premake5.lua
@@ -5,12 +5,14 @@ project "VinlandChronicles"
     targetdir ("%{wks.location}/bin/" .. outDir)
     objdir ("%{wks.location}/obj/" .. outDir)
 
-    links {"VinlandCore", "FLECS"}
-
+    links {"VinlandCore", "FLECS", "Raylib", "pthread", "GL", "m", "dl", "rt", "X11"}
     includedirs {
     "%{prj.location}/include", 
     "%{wks.location}/vinland/include",
-    "%{wks.location}/vinland/vendor/flecs/include"
+    "%{wks.location}/vinland/vendor/flecs/include",
+    "%{wks.location}/vinland/vendor/raylib/src",
+    "%{wks.location}/vinland/vendor/raylib/src/external",
+    "%{wks.location}/vinland/vendor/raylib/src/external/glfw/include"
     }
     files {"%{prj.location}/src/**.c"}
     buildoptions {"-std=c99"}
--- chronicles/src/main.c
+++ chronicles/src/main.c
@@ -1,12 +1,26 @@
 #include "flecs.h"
+#include "raylib.h"
 #include "vinland/vinland.h"
 
 int main(void) {
-    ecs_world_t *world;
+  ecs_world_t *world;
 
-    world = V_InitCore();
-    V_Run(world);
-    V_CleanUp();
+  world = V_InitCore();
+  V_Run(world);
+  V_CleanUp();
+
+  InitWindow(640, 480, "TEST");
+
+  while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(WHITE);
+        DrawText("TEST", 10, 10, 20, LIGHTGRAY);
+
+        EndDrawing();
+  }
+
+  CloseWindow();
 
   return 0;
 }
