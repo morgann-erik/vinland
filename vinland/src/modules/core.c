79706dcf75278d32edbf347fb0e099b5b597094d
margay <morgann.eriksson@proofofconcept.space>
2024-02-12 20:46:13 +0100

    Move V_Position component definition to core module

--- vinland/src/modules/core.c
+++ vinland/src/modules/core.c
@@ -1,16 +1,19 @@
 #include "vinland/internal/modules/core.h"
 #include "flecs.h"
 #include "flecs/addons/flecs_c.h"
+#include "vinland/components/renderer/position.h"
 #include "vinland/internal/components/core/app-specs.h"
 #include "vinland/internal/core/startup-raylib.h"
 #include "vinland/log.h"
 
 ECS_COMPONENT_DECLARE(v_ApplicationSpecs);
+ECS_COMPONENT_DECLARE(V_Position);
 
 void v_CoreModuleImport(ecs_world_t *world) {
   ECS_MODULE(world, V_CoreModule);
 
   ECS_COMPONENT_DEFINE(world, v_ApplicationSpecs);
+  ECS_COMPONENT_DEFINE(world, V_Position);
 
   ECS_SYSTEM(world, v_StartupRaylib, EcsOnStart, v_ApplicationSpecs);
 
--- vinland/src/modules/renderer.c
+++ vinland/src/modules/renderer.c
@@ -1,17 +1,14 @@
 #include "vinland/internal/modules/renderer.h"
 #include "flecs.h"
-#include "vinland/components/renderer/position.h"
 #include "vinland/internal/renderer/renderer.h"
 #include "vinland/log.h"
 
-ECS_COMPONENT_DECLARE(V_Position);
+extern ECS_COMPONENT_DECLARE(V_Position);
 
 void v_RendererModuleImport(ecs_world_t *world) {
   ECS_MODULE(world, V_RendererModule);
 
-  ECS_COMPONENT_DEFINE(world, V_Position);
-
   ECS_SYSTEM(world, Render, EcsOnUpdate, V_Position);
 
-  V_LogDebug("Renderer Module imported");
+  V_LogDebug("Renderer module imported");
 }
