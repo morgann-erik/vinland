8497b75c417558020f41b1a6186e1317a7da6d1f
margay <morgann.eriksson@proofofconcept.space>
2024-02-12 20:27:30 +0100

    Implement app specs component

---
+++ vinland/include/vinland/internal/components/core/app-specs.h
@@ -0,0 +1,11 @@
+#ifndef _V_RENDERER_SPECS
+#define _V_RENDERER_SPECS
+
+typedef struct v_ApplicationSpecs {
+    char* WindowTitle;
+
+    int WindowWidth;
+    int WindowHeight;
+} v_ApplicationSpecs;
+
+#endif
